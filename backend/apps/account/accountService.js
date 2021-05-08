528f51aeccbd54452472fc7a8e9e605fa7f4427b
catchups <narraba3@gmail.com>
2021-05-08 13:26:10 +0900

    sysout제거

--- backend/apps/account/accountService.js
+++ backend/apps/account/accountService.js
@@ -1,15 +1,14 @@
 const axios = require('axios')
 
 let getAccountInfo = async function(account, callbak){
+    
     console.log("[service] ------> getAccountInfo")
-    console.log("[service] ------> getAccountInfo")
+
     await axios.get("https://api-cypress.scope.klaytn.com/v1/accounts/"+ account)
         .then(response => {
-            console.log("22222222222")
             callbak({success : true , response: response.data})
         })
         .catch(error => {
-            console.log("3333333333")
             callbak({success : false , message: error.message})
         });
 };
