5fd50ab411f2aba68b05722f5fe23bf4ee405e2e
han2time <rlagks0429@gmail.com>
2021-09-15 22:13:29 +0900

    klaymore 잔고 반영

--- backend/apps/balance/balanceService.js
+++ backend/apps/balance/balanceService.js
@@ -100,17 +100,17 @@ const getBalance = async function(account, callbak) {
               }
               resultAll.totalBalance.push({
                 "address": allAddressList[i],
-                "name": tokenPriceInfo.name,
                 "symbol": tokenPriceInfo.symbol,
+                "name": tokenPriceInfo.name,
                 "balance": balanceSum,
                 "priceUsdt": tokenPriceInfo.price * balanceSum
               })
             }
           }
-          result.lpBalance = resultAll.lpBalance.filter(function (obj) { return obj.priceUsdt > 1 })
-          result.tokenBalance = resultAll.tokenBalance.filter(function (obj) { return obj.priceUsdt > 1 })
-          result.totalBalance = resultAll.totalBalance.filter(function (obj) { return obj.priceUsdt > 1 })
-          result.totalUsdt = resultAll.totalUsdt
+          // result.lpBalance = resultAll.lpBalance.filter(function (obj) { return obj.priceUsdt > 1 })
+          // result.tokenBalance = resultAll.tokenBalance.filter(function (obj) { return obj.priceUsdt > 1 })
+          // result.totalBalance = resultAll.totalBalance.filter(function (obj) { return obj.priceUsdt > 1 })
+          // result.totalUsdt = resultAll.totalUsdt
   
           // 2. KSP Pending Rewards 가져오기 -> 함수화 시켜야함 
           pendingRewards = await this.getPendingRewards(appName, account)
@@ -121,31 +121,111 @@ const getBalance = async function(account, callbak) {
         } else throw new Error("요청한 건수와 결과 건수가 다릅니다")
         
       } else if (appName === 'klaymore') {
-        // klaymoreURL = 'https://klaymore-mainnet.du.r.appspot.com/balance/'+account
-        // klaymoreReq = await axios.get(klaymoreURL)
-        // klaymoreResult = klaymoreReq.data['tokens']
-        // console.log(klaymoreResult)
+        klaymoreURL = 'https://klaymore-mainnet.du.r.appspot.com/balance/'+account
+        klaymoreReq = await axios.get(klaymoreURL)
+        klaymoreResult = klaymoreReq.data['tokens']
         
-        // for (const tokenName in klaymoreResult) {
-        //   if (tokenName === 'HOUSE') {
-        //     console.log("HOUSE 단일 스테이킹")
+        let pendingMore = {'WOOD':0, 'HOUSE':0, 'AKLAY':0}
 
-        //     tokenAddress = '0x158BeFF8C8cDEbD64654ADD5F6A1d9937e73536c'
-        //     tokenDecimals = KLAYSWAP_TOKEN_INFO[tokenAddress]['decimals']
-        //     tokenAmount = klaymoreResult[tokenName].staked / 10**tokenDecimals
-        //     console.log(tokenAmount)
-        //     console.log(tokenPrice)
-          
-        //   } else if (tokenName === 'AKLAY') {
-        //     console.log("AKLAY 단일 스테이킹")
-        //   } else if (['KLAYAKLAYLP', 'KLAYHOUSELP', 'AKLAYHOUSELP', 'HOUSEWOODLP'].includes(tokenName)) {
-        //     console.log("LP 스테이킹")
-        //   }
-        // }
-        // console.log(result.tokenBalance)
-        continue
+        for (const tokenName in klaymoreResult) {
+          if (tokenName === 'HOUSE' || tokenName === 'AKLAY' ) { // 단일 스테이킹
+            tokenAddress = tokenName === 'HOUSE'? '0x158BeFF8C8cDEbD64654ADD5F6A1d9937e73536c' : '0x74BA03198FEd2b15a51AF242b9c63Faf3C8f4D34'
+            tokenDecimals = KLAYSWAP_TOKEN_INFO[tokenAddress]['decimals']
+            tokenAmount = klaymoreResult[tokenName].staked / 10**tokenDecimals
+            
+            pendingMore['HOUSE'] += Number(klaymoreResult[tokenName]['CHOUSE'])/10**18 + Number(klaymoreResult[tokenName]['CVHOUSE'])/10**18
+            if ( tokenName === 'HOUSE') {
+              pendingMore['AKLAY'] += Number(klaymoreResult[tokenName]['CAKLAY'])/10**18
+              pendingMore['WOOD'] += Number(klaymoreResult[tokenName]['CWOOD'])/10**18
+            }
+
+            searchKhouseToken = resultAll.tokenBalance.filter(function (obj) {
+              return (obj.address === tokenAddress)
+            })
+            if (searchKhouseToken.length > 0) {
+              for (let i = 0; i < resultAll.tokenBalance.length; i++) {
+                const el = resultAll.tokenBalance[i];
+                if(el.address === tokenAddress) {
+                  el.balance += tokenAmount
+                  el.priceUsdt += tokenPrice[tokenAddress]['price'] * tokenAmount
+                }
+              }
+              for (let i = 0; i < resultAll.totalBalance.length; i++) {
+                const el = resultAll.totalBalance[i];
+                if(el.address === tokenAddress) {
+                  el.balance += tokenAmount
+                  el.priceUsdt += tokenPrice[tokenAddress]['price'] * tokenAmount
+                }
+              }
+            } else {
+              resultAll.tokenBalance.push({
+                "address": tokenAddress,
+                "symbol": KLAYSWAP_TOKEN_INFO[tokenAddress]['symbol'],
+                "name": KLAYSWAP_TOKEN_INFO[tokenAddress]['name'],
+                "balance": tokenAmount,
+                "priceUsdt": tokenPrice[tokenAddress]['price'] * tokenAmount
+              })
+              resultAll.totalBalance.push({
+                "address": tokenAddress,
+                "symbol": KLAYSWAP_TOKEN_INFO[tokenAddress]['symbol'],
+                "name": KLAYSWAP_TOKEN_INFO[tokenAddress]['name'],
+                "balance": tokenAmount,
+                "priceUsdt": tokenPrice[tokenAddress]['price'] * tokenAmount
+              })
+            }
+            resultAll.totalUsdt += tokenPrice[tokenAddress]['price'] * tokenAmount
+
+          } else if (['KLAYAKLAYLP', 'KLAYHOUSELP', 'AKLAYHOUSELP', 'HOUSEWOODLP'].includes(tokenName)) { // lp 스테이킹
+            
+            if (tokenName === 'KLAYAKLAYLP') lpTokenAddress = '0xE74C8D8137541C0EE2C471cdAF4DCf03C383Cd22'
+            else if (tokenName === 'KLAYHOUSELP') lpTokenAddress = '0x8ad37f3f3fB663A67c7140947F80894a973B789B'
+            else if (tokenName === 'AKLAYHOUSELP') lpTokenAddress = '0xafEDb9fd20d0007a13DcD685a89825cd2866f0fc'
+            else if (tokenName === 'HOUSEWOODLP') lpTokenAddress = '0xF05200168fF2e1b0848Cf4981184017F94710Fa6'
+
+            lpTokenDecimals = KLAYSWAP_LPTOKEN_INFO[lpTokenAddress]['decimals']
+            lpTokenAmount = klaymoreResult[tokenName].staked / 10**tokenDecimals
+            
+            pendingMore['HOUSE'] += Number(klaymoreResult[tokenName]['CHOUSE'])/10**18 + Number(klaymoreResult[tokenName]['CVHOUSE'])/10**18
+
+            tokens = [lpPrice[lpTokenAddress]['tokenA'], lpPrice[lpTokenAddress]['tokenB']]
+            for (let i = 0; i < tokens.length; i++) {
+              tokenAmount = i===0 ? lpTokenAmount*lpPrice[lpTokenAddress]['tokenAUnit'] : lpTokenAmount*lpPrice[lpTokenAddress]['tokenBUnit']
+              searchKhouseToken = resultAll.totalBalance.filter(function (obj) {
+                return (obj.address === tokens[i])
+              })
+              
+              if (searchKhouseToken.length > 0) {
+                for (let j = 0; j < resultAll.totalBalance.length; j++) {
+                  const el = resultAll.totalBalance[j];
+                  if(el.address === tokens[i]) {
+                    el.balance += tokenAmount
+                    el.priceUsdt += tokenPrice[tokens[i]]['price'] * tokenAmount
+                  }
+                }
+              } else {
+                resultAll.totalBalance.push({
+                  "address": tokens[i],
+                  "symbol": KLAYSWAP_TOKEN_INFO[tokens[i]]['symbol'],
+                  "name": KLAYSWAP_TOKEN_INFO[tokens[i]]['name'],
+                  "balance": tokenAmount,
+                  "priceUsdt": tokenPrice[tokens[i]]['price'] * tokenAmount
+                })
+              }
+              resultAll.totalUsdt += tokenPrice[tokens[i]]['price'] * tokenAmount
+            }
+          }
+        }
+        result.pendingRewards['KLAYMORE'] = pendingMore
+
       } else throw new Error("거래소 목록 오류")
     }
+
+    result.lpBalance = resultAll.lpBalance.filter(function (obj) { return obj.priceUsdt > 1 })
+    result.tokenBalance = resultAll.tokenBalance.filter(function (obj) { return obj.priceUsdt > 1 })
+    result.totalBalance = resultAll.totalBalance.filter(function (obj) { return obj.priceUsdt > 1 })
+    result.totalUsdt = resultAll.totalUsdt
+    // console.log(result.pendingRewards)
+
     callbak({ success: true, response: result })
   } catch (error) {
     callbak({success : false , message: error.message})
