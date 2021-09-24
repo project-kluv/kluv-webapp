f776bf32112a5e7e8222ea2de66708330a47891f
han2time <rlagks0429@gmail.com>
2021-09-24 17:05:34 +0900

    token update & add time to price service

--- backend/apps/pool/poolService.js
+++ backend/apps/pool/poolService.js
@@ -89,79 +89,82 @@ const getAllTokenPrice = async function (appName, callbak){
 const getTokenPriceInApp = function (appName, lpPools) {
   allTokenPriceInApp ={token:{}, lp:{}}
   const tokenInfo = poolModel.getTokenInfo(appName)
-    for (const tokenAddress in tokenInfo) {
-      if (Object.hasOwnProperty.call(tokenInfo, tokenAddress)) {
-        allTokenPriceInApp.token[tokenAddress] = {
-          decimals : tokenInfo[tokenAddress].decimals,
-          name : tokenInfo[tokenAddress].name,
-          symbol : tokenInfo[tokenAddress].symbol,
-          price : 0
-        }
-      }
-    }
-    // 1. get price using USDT pair : 0xcee8faf64bb97a73bb51e115aa89c17ffa8dd167
-    allTokenPriceInApp.token['0xceE8FAF64bB97a73bb51E115Aa89C17FfA8dD167'].price = 1
-    calcTokenPrice(lpPools['data'], '0xceE8FAF64bB97a73bb51E115Aa89C17FfA8dD167')
-    // 2. get pirce using klay pair : 0x0000000000000000000000000000000000000000
-    calcTokenPrice(lpPools['data'], '0x0000000000000000000000000000000000000000')
-      // 3. get price using others.
-    calcTokenPrice(lpPools['data'], '0x588C62eD9aa7367d7cd9C2A9aaAc77e44fe8221B'); // Agov -> HINT
-    calcTokenPrice(lpPools['data'], '0x34d21b1e550D73cee41151c77F3c73359527a396'); // KETH -> KICX
-    calcTokenPrice(lpPools['data'], '0x158BeFF8C8cDEbD64654ADD5F6A1d9937e73536c'); // HOUSE -> WOOD
-    calcTokenPrice(lpPools['data'], '0xe950bdcFa4d1e45472E76cf967Db93dBfc51Ba3E'); // KAI -> 합성자산
-    
+  const currentTime = new Date()
 
-    // get LP price
-    for (let i = 0; i < lpPools['data'].length; i++) {
-      const lpPool = lpPools['data'][i];
-      tokenAUnit = lpPool.tokenAAmount/lpPool.totalSupply
-      tokenBUnit = lpPool.tokenBAmount/lpPool.totalSupply
-      
-      allTokenPriceInApp.lp[lpPool.address] = {
-        "totalSupply": lpPool.totalSupply,
-        "name": lpPool.name,
-        "tokenA": lpPool.tokenA,
-        "tokenB": lpPool.tokenB,
-        "tokenAName": lpPool.tokenAName,
-        "tokenBName": lpPool.tokenBName,
-        "tokenASymbol": lpPool.tokenASymbol,
-        "tokenBSymbol": lpPool.tokenBSymbol,
-        "tokenAUnit": tokenAUnit,
-        "tokenBUnit": tokenBUnit,
-        "price": tokenAUnit*(10**lpPool.decimals/10**lpPool.tokenADecimals)*allTokenPriceInApp.token[lpPool.tokenA].price + 
-                tokenBUnit*(10**lpPool.decimals/10**lpPool.tokenBDecimals)*allTokenPriceInApp.token[lpPool.tokenB].price,
-        "decimals": Number(lpPool.decimals)
-      }
-    }
-    return allTokenPriceInApp
-
-    function calcTokenPrice(lpPools, standardAddress){
-      const pairs = lpPools.filter(function(obj){
-        condA = obj.tokenA == standardAddress;
-        condB = obj.tokenB == standardAddress;
-        return condA || condB
-      });
-      for (let i = 0; i < pairs.length; i++) {
-        const lp = pairs[i];
-        if(lp.tokenA == standardAddress) {
-          standardAmount = lp.tokenAAmount/(10**lp.tokenADecimals)
-          tokenAmount = lp.tokenBAmount/(10**lp.tokenBDecimals)
-          tokenAddress = lp.tokenB
-        } else {
-          standardAmount = lp.tokenBAmount/(10**lp.tokenBDecimals)
-          tokenAmount = lp.tokenAAmount/(10**lp.tokenADecimals)
-          tokenAddress = lp.tokenA
-        }
-        
-        if (standardAddress == '0xceE8FAF64bB97a73bb51E115Aa89C17FfA8dD167'){
-          price = standardAmount/tokenAmount
-        } else{
-          price = allTokenPriceInApp.token[standardAddress].price * standardAmount/tokenAmount
-        }
-        allTokenPriceInApp.token[tokenAddress].price = allTokenPriceInApp.token[tokenAddress].price == 0 ? price : allTokenPriceInApp.token[tokenAddress].price;
+  for (const tokenAddress in tokenInfo) {
+    if (Object.hasOwnProperty.call(tokenInfo, tokenAddress)) {
+      allTokenPriceInApp.token[tokenAddress] = {
+        decimals : tokenInfo[tokenAddress].decimals,
+        name : tokenInfo[tokenAddress].name,
+        symbol: tokenInfo[tokenAddress].symbol,
+        datetime : currentTime.toISOString(),
+        price : 0
       }
     }
   }
+  // 1. get price using USDT pair : 0xcee8faf64bb97a73bb51e115aa89c17ffa8dd167
+  allTokenPriceInApp.token['0xceE8FAF64bB97a73bb51E115Aa89C17FfA8dD167'].price = 1
+  calcTokenPrice(lpPools['data'], '0xceE8FAF64bB97a73bb51E115Aa89C17FfA8dD167')
+  // 2. get pirce using klay pair : 0x0000000000000000000000000000000000000000
+  calcTokenPrice(lpPools['data'], '0x0000000000000000000000000000000000000000')
+    // 3. get price using others.
+  calcTokenPrice(lpPools['data'], '0x588C62eD9aa7367d7cd9C2A9aaAc77e44fe8221B'); // Agov -> HINT
+  calcTokenPrice(lpPools['data'], '0x34d21b1e550D73cee41151c77F3c73359527a396'); // KETH -> KICX
+  calcTokenPrice(lpPools['data'], '0x158BeFF8C8cDEbD64654ADD5F6A1d9937e73536c'); // HOUSE -> WOOD
+  calcTokenPrice(lpPools['data'], '0xe950bdcFa4d1e45472E76cf967Db93dBfc51Ba3E'); // KAI -> 합성자산
+  
+
+  // get LP price
+  for (let i = 0; i < lpPools['data'].length; i++) {
+    const lpPool = lpPools['data'][i];
+    tokenAUnit = lpPool.tokenAAmount/lpPool.totalSupply
+    tokenBUnit = lpPool.tokenBAmount/lpPool.totalSupply
+    
+    allTokenPriceInApp.lp[lpPool.address] = {
+      "totalSupply": lpPool.totalSupply,
+      "name": lpPool.name,
+      "tokenA": lpPool.tokenA,
+      "tokenB": lpPool.tokenB,
+      "tokenAName": lpPool.tokenAName,
+      "tokenBName": lpPool.tokenBName,
+      "tokenASymbol": lpPool.tokenASymbol,
+      "tokenBSymbol": lpPool.tokenBSymbol,
+      "tokenAUnit": tokenAUnit,
+      "tokenBUnit": tokenBUnit,
+      "price": tokenAUnit*(10**lpPool.decimals/10**lpPool.tokenADecimals)*allTokenPriceInApp.token[lpPool.tokenA].price + 
+              tokenBUnit*(10**lpPool.decimals/10**lpPool.tokenBDecimals)*allTokenPriceInApp.token[lpPool.tokenB].price,
+      "decimals": Number(lpPool.decimals)
+    }
+  }
+  return allTokenPriceInApp
+
+  function calcTokenPrice(lpPools, standardAddress){
+    const pairs = lpPools.filter(function(obj){
+      condA = obj.tokenA == standardAddress;
+      condB = obj.tokenB == standardAddress;
+      return condA || condB
+    });
+    for (let i = 0; i < pairs.length; i++) {
+      const lp = pairs[i];
+      if(lp.tokenA == standardAddress) {
+        standardAmount = lp.tokenAAmount/(10**lp.tokenADecimals)
+        tokenAmount = lp.tokenBAmount/(10**lp.tokenBDecimals)
+        tokenAddress = lp.tokenB
+      } else {
+        standardAmount = lp.tokenBAmount/(10**lp.tokenBDecimals)
+        tokenAmount = lp.tokenAAmount/(10**lp.tokenADecimals)
+        tokenAddress = lp.tokenA
+      }
+      
+      if (standardAddress == '0xceE8FAF64bB97a73bb51E115Aa89C17FfA8dD167'){
+        price = standardAmount/tokenAmount
+      } else{
+        price = allTokenPriceInApp.token[standardAddress].price * standardAmount/tokenAmount
+      }
+      allTokenPriceInApp.token[tokenAddress].price = allTokenPriceInApp.token[tokenAddress].price == 0 ? price : allTokenPriceInApp.token[tokenAddress].price;
+    }
+  }
+}
 
 
 
