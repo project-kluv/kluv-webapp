65a599329ebae36c6c2dda0846b619f12c87b1f5
jhwandev <jhwan.dev@gmail.com>
2021-10-08 00:01:48 +0900

    footer 수정, 기부주소

--- frontend/src/containers/layouts/common/footer.vue
+++ frontend/src/containers/layouts/common/footer.vue
@@ -3,7 +3,8 @@
     <!-- Footer Start -->
     <div class="flex-grow-1"></div>
     <div class="app-footer">
-      <strong> &copy; Kluv.me</strong>
+      Donate Klaytn Address : <span class="clickable" @click="clip();"><u>0xB55d37D4D42437E72E9D55e3Ac37ae6c1bfcF444</u></span>
+      
       <!-- <div class="row"> -->
         <!-- <div class="col-md-9">
           <p><strong>Kluv.me</strong></p>
@@ -14,7 +15,8 @@
       >
         <div class="d-flex align-items-center">
           <div>
-              <p class="m-0">All rights reserved</p>
+              <!-- <p class="m-0">All rights reserved</p> -->
+              <strong> &copy; Kluv.me 2021. All rights reserved</strong>
           </div>
           <span class="flex-grow-1"></span>
         </div>
@@ -24,11 +26,36 @@
   </div>
 </template> 
 <script>
+
+
+
 export default {
   data() {
     return {};
   },
-  methods: {}
+  methods: {
+    makeToast(variant = null, msg) {
+      this.$bvToast.toast(msg, {
+        title: ` ${variant || "default"}`,
+        variant: variant,
+        solid: true
+      });
+    },
+
+    clip(){
+      const t = document.createElement("textarea")
+      document.body.appendChild(t)
+      t.value = '0xB55d37D4D42437E72E9D55e3Ac37ae6c1bfcF444'
+      t.select()
+      document.execCommand('copy')
+      document.body.removeChild(t)
+
+      this.makeToast("success", "복사가 완료되었습니다.");
+
+
+    
+    }
+  }
 };
 </script>
 
